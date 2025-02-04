95ed7405aa1cfe9b80019f40dcb09624a208abdb
Misky-Abdullahi <144022670+Misky-Abdullahi@users.noreply.github.com>
2025-02-04 13:27:11 +0300

    Update dashboard_page.dart
    seccon one

--- dashboard_page.dart
+++ dashboard_page.dart
@@ -1,46 +1,24 @@
-import 'package:flutter/material.dart';
-import 'database_helper.dart';
-
-class DashboardPage extends StatefulWidget {
-  final String username;
-  final String role;
-
-  DashboardPage({required this.username, required this.role});
-
-  @override
-  _DashboardPageState createState() => _DashboardPageState();
-}
-
-class _DashboardPageState extends State<DashboardPage> {
-  int patientCount = 0;
-  int doctorCount = 0;
-  int appointmentCount = 0;
-  int userCount = 0;
-
-  final List<String> bannerImages = [
-    "img/1.jpg",
-    "img/2.jpg",
-    "img/3.jpg",
-  ];
-
-  int currentBannerIndex = 0;
-
-  @override
+@override
   void initState() {
     super.initState();
-    _loadCounts();
-  }
+    _pages = [
+      DashboardPage(username: widget.username, role: widget.role),
+      PatientPage(),
+      DoctorPage(),
+      AppointmentPage(),
+    ];
 
-  Future<void> _loadCounts() async {
-    int pCount = await DatabaseHelper.getPatientCount();
-    int dCount = await DatabaseHelper.getDoctorCount();
-    int aCount = await DatabaseHelper.getAppointmentCount();
-    int uCount = await DatabaseHelper.getUserCount();
+    _navItems = [
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Patients'),
+      BottomNavigationBarItem(icon: Icon(Icons.medical_services), label: 'Doctors'),
+      BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: 'Appointments'),
+    ];
 
-    setState(() {
-      patientCount = pCount;
-      doctorCount = dCount;
-      appointmentCount = aCount;
-      userCount = uCount;
-    });
+    if (widget.role.toLowerCase() != "user") {
+      _pages.add(UserPage());
+      _navItems.add(
+        BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle), label: 'Users'),
+      );
+    }
   }
