18e963eb3ef0300a7f3e0cb5c0392f28039000e5
Misky-Abdullahi <144022670+Misky-Abdullahi@users.noreply.github.com>
2025-02-04 13:28:02 +0300

    Update dashboard_page.dart
    thirt one

--- dashboard_page.dart
+++ dashboard_page.dart
@@ -1,24 +1,26 @@
-@override
-  void initState() {
-    super.initState();
-    _pages = [
-      DashboardPage(username: widget.username, role: widget.role),
-      PatientPage(),
-      DoctorPage(),
-      AppointmentPage(),
-    ];
-
-    _navItems = [
-      BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-      BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Patients'),
-      BottomNavigationBarItem(icon: Icon(Icons.medical_services), label: 'Doctors'),
-      BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: 'Appointments'),
-    ];
-
-    if (widget.role.toLowerCase() != "user") {
-      _pages.add(UserPage());
-      _navItems.add(
-        BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle), label: 'Users'),
-      );
-    }
-  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard (${widget.role})'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: _logout,
+          ),
+        ],
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10,
+              spreadRadius: 2,
+            ),
+          ],
+        ),
